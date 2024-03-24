9bda31abae55877ad2ce4782fdcc5a9629fdc6db
yesman <percev.yevgen@gmail.com>
2024-03-25 01:28:52 +0200

    Add project files.

---
+++ Elongear/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Elongear
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Elongear/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Elongear
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
